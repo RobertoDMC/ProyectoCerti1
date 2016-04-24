23d31ca5b4cef8d4ead0be7c0d8461fbf2574ea0
Mauricio Fernandez <maury844@gmail.com>
2016-04-24 18:25:34 -0400

    Splitted JS for modal panel

---
+++ Html/modal.js
@@ -0,0 +1,11 @@
+
+function openModal() {
+	    
+	    var modal = document.getElementById("openModal");
+	    modal.style.zIndex = "20";	
+}
+
+function closeModal() {
+	var modal = document.getElementById("openModal");
+	modal.style.zIndex = "-10";
+}
