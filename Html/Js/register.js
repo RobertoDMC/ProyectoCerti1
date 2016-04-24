c357df0308fc15cf465f45657a7c49a20489fd23
Roberto Molina <robert.dmc@hotmail.com>
2016-04-24 19:11:14 -0400

    adicion de sexo al JSON del signup

--- Html/Js/register.js
+++ Html/Js/register.js
@@ -1,10 +1,26 @@
 function register(){
+	var sex;
+
+		if(document.getElementById('male').checked)
+		{
+			sex = "Male";
+		}
+		else
+		{
+			sex = "Female";
+		}
+
+
+
 		var res = '{'+'"datos":{'+
 		'"username":' + '"'+ document.forms["registerForm"]["username"].value +'"'+ ',' +
 		'"password":' + '"'+ document.forms["registerForm"]["password"].value +'"'+ ',' +
 		'"email":'    + '"'+ document.forms["registerForm"]["email"].value    +'"'+ ',' +
+		'"sex":'    + '"'+   sex    +'"'+ ',' +
 		'"birthday":' + '"'+ document.forms["registerForm"]["birthday"].value +'"'+ 
 		'}}';
+
+
 
 		window.alert(res);
 		
