51af777898718ccaecbcec299d49b3d7a006ba7d
Roberto Molina <robert.dmc@hotmail.com>
2016-05-17 23:13:39 -0400

    Try to solve problems

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-var port = 8080;
-
-app.listen(port);
-console.log("Server running on " + port);
+app.listen(process.env.PORT || 3000, function(){
+  //console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
